56aeca44e10e0201cd2aa0fd82010cf17c9981df
IAMZAID22 <73696883+IAMZAID22@users.noreply.github.com>
2021-01-05 09:02:21 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -79,6 +79,8 @@ function setup(){
 function draw(){
   background(back);
   if(GameState === PLAY){
+    textSize(25);
+    fill("white");
   text("Coins:"+coins,50,50);
  if(keyDown("w") || keyDown("up")) {
         pla.y = pla.y -22;
@@ -100,7 +102,8 @@ function draw(){
     pla.visible = false;
   }
   if(pla.isTouching(plt8)){
-    textSize(5);
+    textSize(75);
+    fill("white")
     text("You Won",950,200);
   }
   if(pla.isTouching(c1)  ){
